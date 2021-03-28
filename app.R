ee47a8e41ed6fb771920b033591dd6e07218c122
krmuth <krmuth@gmail.com>
2021-03-28 17:01:59 +0100

    Build cors network

--- app.R
+++ app.R
@@ -21,31 +21,20 @@ tidy_books <- original_books %>%
   unnest_tokens(word, text) %>%
   anti_join(stop_words)
 
-tidy_emma <- original_books %>%
-  filter(title == "Emma") %>% 
-  unnest_tokens(word, text) %>%
-  anti_join(stop_words)
+tidy_emma <- tidy_books %>%
+  filter(title == "Emma")  
 
-tidy_bleak_house <- original_books %>%
-  filter(title == "Bleak House") %>%
-  unnest_tokens(word, text) %>%
-  anti_join(stop_words)
+tidy_bleak_house <- tidy_books %>%
+  filter(title == "Bleak House")
 
-tidy_heart_of_darkness <- original_books %>%
-  filter(title == "Heart of Darkness") %>%
-  unnest_tokens(word, text) %>%
-  anti_join(stop_words)
+tidy_heart_of_darkness <- tidy_books %>%
+  filter(title == "Heart of Darkness") 
 
-tidy_jane_eyre <- original_books %>%
-  filter(title == "Jane Eyre: An Autobiography") %>%
-  unnest_tokens(word, text) %>%
-  anti_join(stop_words)
+tidy_jane_eyre <- tidy_books %>%
+  filter(title == "Jane Eyre: An Autobiography") 
 
-tidy_moll_flanders <- original_books %>%
-  filter(title == "The Fortunes and Misfortunes of the Famous Moll Flanders") %>%
-  unnest_tokens(word, text) %>%
-  anti_join(stop_words)
-
+tidy_moll_flanders <- tidy_books %>%
+  filter(title == "The Fortunes and Misfortunes of the Famous Moll Flanders") 
 
 
 # User interface ----
@@ -79,7 +68,7 @@ tidy_moll_flanders <- original_books %>%
                   sliderInput("chart_max",
                               label = "Number of words",
                               min = 10, max = 30, value = 20)),
-         tabPanel("Correlations", plotOutput("cor")))
+         tabPanel("Correlations", plotOutput("pairs")))
        )
      
    )
@@ -147,38 +136,52 @@ server <- function(input, output) {
   
   # Correlation tab ----
   
-  # Select dataset
+  output$pairs <- renderPlot({
+    
+    # Select dataset
   
-  output$cor <- renderPlot({
-  
-  books <- switch(input$var, 
-                  "All" = tidy_books,
+  books <- switch(input$var,
+                  "All" = tidy_moll_flanders,
                   "Moll Flanders" = tidy_moll_flanders,
                   "Emma" = tidy_emma,
                   "Jane Eyre" = tidy_jane_eyre,
                   "Bleak House" = tidy_bleak_house,
                   "Heart of Darkness" = tidy_heart_of_darkness)
-  
+
   section_words <- books %>%
     mutate(section = row_number() %/% 10) %>%
     filter(section > 0)
-  
+
   word_cors <- section_words %>%
+    group_by(word) %>%
     filter(n() >= 20) %>%
     pairwise_cor(word, section, sort = TRUE)
-  
+
   word_cors %>%
-    filter(item1 %in% c("time", "hand", "looked", "day")) %>%
-    group_by(item1) %>%
-    slice(1:6) %>%
-    ungroup() %>%
-    mutate(item2 = reorder(item2, correlation)) %>%
-    ggplot(aes(item2, correlation)) +
-    geom_bar(stat = "identity") +
-    facet_wrap(~ item1, scales = "free") +
-    coord_flip()
+    filter(correlation > .15) %>%
+    graph_from_data_frame() %>%
+    ggraph(layout = "fr") +
+    geom_edge_link(aes(edge_alpha = correlation), show.legend = FALSE) +
+    geom_node_point(color = "lightblue", size = 5) +
+    geom_node_text(aes(label = name), repel = TRUE) +
+    theme_void()
 
   })
+  
+  # word_cors %>%
+  #   filter(item1 %in% c("time", "day", "looked", "house")) %>%
+  #   group_by(item1) %>%
+  #   slice(1:6) %>%
+  #   ungroup() %>%
+  #   mutate(item2 = reorder(item2, correlation)) %>%
+  #   ggplot(aes(item2, correlation)) +
+  #   geom_bar(stat = "identity") +
+  #   facet_wrap(~ item1, scales = "free") +
+  #   coord_flip()
+  # 
+  # })
+
+
   
   # word_pairs <- section_words %>%
   #   pairwise_count(word, section, sort = TRUE)
